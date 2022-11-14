b8baafe6e0ace6b5ae5e7c291b55a584dd7ab47e
msalinero <msalinero85@gmail.com>
2022-11-14 13:41:19 +0100

    Update Main.m

--- src/Main.m
+++ src/Main.m
@@ -1,4 +1,4 @@
 %%
 
 %%
-[out] = GPR_ARTMO_to_GEE('C:\Users\1\Desktop\Drive\IPL\ARTMOtoGEE\MLRAmodel_LAI_BOA_ref_GPR_AL_EBD_161samples_S2_10B_MNI.mat','_LAI');
+[out] = GPR_ARTMO_to_GEE('path_to_mat_model_file','_LAI');
